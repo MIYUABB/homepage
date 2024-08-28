9e16501a3d48ce353166ff5437818fe8c5625dbb
MIYUABB <cyril.llopis@gmail.com>
2024-08-28 19:24:14 +0200

    work

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import Navbar from "../comoponents/navbar"
 import Footer from "../comoponents/footer";
 import Introduction from "../comoponents/introduction";
 import Quote from "../comoponents/quote";
+import Img from "../comoponents/img";
 
 function Home() {
     document.title = 'Cyril Llopis'
@@ -10,7 +11,10 @@ function Home() {
         <div className="flex flex-col bg-body-col">
             <Navbar />
             <Introduction title="Willkommen auf meiner Homepage" img="Teide_Moon.jpg" alt="Vollmond bei der Schlafstelle auf dem El Teide" bg={false}/>
-            <Quote quote="Informatiker. Kletterer. Motorradfahrer. Wer bin ich?"/>
+            <div className="bg-shade-secondary w-fit mx-auto p-4 mt-10 rounded-l">
+                <Img/>
+                <Quote quote="Informatiker. Kletterer. Motorradfahrer. Wer bin ich?"/>
+            </div>
             <Footer />
         </div>
     );

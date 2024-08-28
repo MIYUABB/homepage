938ade8826b0e56713e476900132af47c7e22f0c
MIYUABB <cyril.llopis@gmail.com>
2024-08-28 18:51:47 +0200

    fix

--- src/comoponents/introduction.tsx
+++ src/comoponents/introduction.tsx
@@ -6,7 +6,7 @@ function Introduction(props: { title: string, img: any, alt: string, bg:boolean}
     if(props.bg){
         return (
             <Fragment>
-                <img src={require("../images/" + props.img)} alt={props.alt} className="h-screen w-auto z-0 mt-0"/>
+                <img src={require("../images/" + props.img)} alt={props.alt} className="object-cover h-screen"/>
                 <div className="z-50 mt-80 absolute place-self-center h-36">
                     <div className="text-text-col text-4xl">
                         <h2 className="bg-body-col bg-opacity-50 p-4 rounded-xl">{props.title}</h2>
--- src/comoponents/navbar.tsx
+++ src/comoponents/navbar.tsx
@@ -7,8 +7,10 @@ function Navbar() {
                 <a href="/" className="">Cyril</a>
             </h1>
             <nav className="absolute inset-y-0 content-center right-8 text-xl text-text-col">
+                <a href="/" className="p-2 hover:opacity-100 hover:underline opacity-80">Startseite</a>
                 <a href="/career" className="p-2 hover:opacity-100 hover:underline opacity-80">Werdegang</a>
                 <a href="/hobbys" className="p-2 hover:opacity-100 hover:underline opacity-80">Hobbys</a>
+                <a href="https://github.com/MIYUABB?tab=repositories" className="p-2 hover:opacity-100 hover:underline opacity-80" target="_blank" rel="noreferrer">GitHub</a>
             </nav>
         </div>
     )

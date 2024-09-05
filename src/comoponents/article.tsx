4ba7f9f4b5eed823da249ddc2ce5c8459d5a051b
MIYUABB <cyril.llopis@gmail.com>
2024-09-05 17:00:47 +0200

    responsivenes article.tsx

--- src/comoponents/article.tsx
+++ src/comoponents/article.tsx
@@ -3,10 +3,10 @@ import React from "react"
 function Article(props: { title: string , text: string, img: string, imgleft: boolean, alt:string }){
     if(props.imgleft) {
         return(
-            <div className="mx-20 lg lg:mx-50 z-50 mt-10">
+            <div className="mx-6 md:mx-20 lg:mx-50 z-50 md:mt-10 mt-5">
                 <div className="h-min grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 bg-gradient-to-br from-shade-primary to-shade-secondary md:p-2 lg:p-4 rounded-lg">
-                    <img src={require("../images/" + props.img)} alt={props.alt} className="rounded h-fit place-self-center lg:place-self-start max-h-96"/>
-                    <div className="p-8 rounded w-fit h-fit place-self-center text-2xl lg:text-lg">
+                    <img src={require("../images/" + props.img)} alt={props.alt} className="rounded h-fit place-self-center lg:place-self-start w-11/12 lg:h-11/12"/>
+                    <div className="p-8 rounded w-fit h-fit place-self-center text-lg">
                         <h2 className="text-title-col text-center lg:text-start lg:text-xl underline">{props.title}</h2>
                         <div className="text-text-col p-2">{props.text}</div>
                     </div>
@@ -15,13 +15,13 @@ function Article(props: { title: string , text: string, img: string, imgleft: bo
         )
     }else {
         return (
-            <div className="mx-20 lg lg:mx-50 z-50 mt-10">
-                <div className="h-min grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 bg-gradient-to-br from-shade-primary to-shade-secondary md:p-2 lg:p-4 rounded-lg">
-                    <div className="p-8 rounded w-fit h-fit place-self-center text-2xl lg:text-lg">
+            <div className="mx-6 md:mx-20 lg:mx-50 z-50 md:mt-10 mt-5">
+                <div className="h-min grid grid-flow-row lg:grid-flow-col grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 bg-gradient-to-br from-shade-primary to-shade-secondary md:p-2 lg:p-4 rounded-lg">
+                    <div className="p-8 rounded w-fit h-fit place-self-center text-lg">
                         <h2 className="text-title-col text-center lg:text-start lg:text-xl underline">{props.title}</h2>
                         <div className="text-text-col p-2">{props.text}</div>
                     </div>
-                    <img src={require("../images/" + props.img)} alt={props.alt} className="rounded h-fit place-self-center lg:place-self-end max-h-96"/>
+                    <img src={require("../images/" + props.img)} alt={props.alt} className="rounded h-fit place-self-center lg:place-self-end pb-4 w-11/12 lg:max-w-96"/>
                 </div>
             </div>
         )

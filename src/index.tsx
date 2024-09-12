c382c6ec634e6f177b6c673f46b4f46a7fe10abd
MIYUABB <cyril.llopis@gmail.com>
2024-09-12 11:04:48 +0200

    index.tsx cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import App from "./App";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-let body = document.querySelector("body")
 
 root.render(
     <App />
